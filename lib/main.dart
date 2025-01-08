16cd37c59c16f206e2793f956267680061b1df14
Heurtault Aubin <aub.heurtault@gmail.com>
2025-01-08 12:09:24 +0100

    feat : add provider and fake routines

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:routine_app/providers/completionProvider.dart';
+import 'package:routine_app/providers/routineProvider.dart';
 import 'package:routine_app/screens/main_screen.dart';
+import 'package:provider/provider.dart';
+import 'db/database.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Assure l'initialisation
+
+  final dbHelper = DatabaseHelper.instance;
+
+  // Faker : Insérer des routines dans la base de données
+  await dbHelper.insertFakeRoutines(10);
+  final routines = await dbHelper.getRoutines();
+  for (final routine in routines) {
+    print(routine);
+  }
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => RoutineProvider()),
+        ChangeNotifierProvider(create: (_) => CompletionProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,7 +37,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MainScreen(),
+      home: const MainScreen(), // Écran principal
     );
   }
 }
