bf776071c6acbcd0d2cef1d8f67edbcf4b8e6ec4
amine <amine.bacha50@gmail.com>
2025-01-09 11:12:04 +0100

    feat : ajout demande de supression

--- lib/screens/routines_screen.dart
+++ lib/screens/routines_screen.dart
@@ -51,7 +51,7 @@ class _RoutineScreenState extends State<RoutineScreen> {
                   IconButton(
                     icon: const Icon(Icons.delete, color: Colors.black),
                     onPressed: () {
-                      routineProvider.deleteRoutine(routine['id']);
+                      _confirmDelete(context, routine['id'], routineProvider);
                     },
                   ),
                 ],
@@ -70,6 +70,36 @@ class _RoutineScreenState extends State<RoutineScreen> {
     );
   }
 
+  // Confirmation avant de supprimer une routine
+  void _confirmDelete(BuildContext context, int id, RoutineProvider routineProvider) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirmer la suppression'),
+          content: const Text('Êtes-vous sûr de vouloir supprimer cette routine ?'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Annuler'),
+            ),
+            TextButton(
+              onPressed: () async {
+                await routineProvider.deleteRoutine(id);
+                Navigator.pop(context);
+              },
+              child: const Text(
+                'Supprimer',
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Formulaire d'ajout/modification
   void _showRoutineForm(BuildContext context, {Map<String, dynamic>? routine, bool isEditing = false}) {
     final routineProvider = Provider.of<RoutineProvider>(context, listen: false);
 
@@ -119,111 +149,128 @@ class _RoutineScreenState extends State<RoutineScreen> {
                 top: 16,
                 bottom: MediaQuery.of(context).viewInsets.bottom,
               ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    isEditing ? 'Modifier Routine' : 'Nouvelle Routine',
-                    style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 16),
-                  TextField(
-                    controller: nameController,
-                    decoration: const InputDecoration(labelText: 'Nom', border: OutlineInputBorder()),
-                  ),
-                  const SizedBox(height: 16),
-                  TextField(
-                    controller: descriptionController,
-                    decoration: const InputDecoration(labelText: 'Description', border: OutlineInputBorder()),
-                  ),
-                  const SizedBox(height: 16),
-                  TextField(
-                    controller: iconController,
-                    decoration: const InputDecoration(labelText: 'Icône', border: OutlineInputBorder()),
-                  ),
-                  const SizedBox(height: 16),
-                  TextField(
-                    controller: startDateController,
-                    decoration: const InputDecoration(labelText: 'Date de début', border: OutlineInputBorder()),
-                  ),
-                  const SizedBox(height: 16),
-                  TextField(
-                    controller: endDateController,
-                    decoration: const InputDecoration(labelText: 'Date de fin', border: OutlineInputBorder()),
-                  ),
-                  const SizedBox(height: 16),
-                  DropdownButtonFormField<int>(
-                    value: selectedFrequencyId,
-                    decoration: const InputDecoration(labelText: 'Fréquence', border: OutlineInputBorder()),
-                    items: frequencies.entries
-                        .map((entry) => DropdownMenuItem(
-                      value: entry.key,
-                      child: Text(entry.value),
-                    ))
-                        .toList(),
-                    onChanged: (value) {
-                      setModalState(() {
-                        selectedFrequencyId = value!;
-                      });
-                    },
-                  ),
-                  const SizedBox(height: 16),
-                  TextField(
-                    controller: TextEditingController(text: recurrence.toString()),
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(labelText: 'Récurrence', border: OutlineInputBorder()),
-                    onChanged: (value) {
-                      setModalState(() {
-                        recurrence = int.parse(value);
-                      });
-                    },
-                  ),
-                  const SizedBox(height: 16),
-                  Wrap(
-                    spacing: 8.0,
-                    children: daysOfWeek.entries.map((entry) {
-                      return ChoiceChip(
-                        label: Text(entry.value),
-                        selected: selectedDays.contains(entry.key),
-                        onSelected: (selected) {
-                          setModalState(() {
-                            if (selected) {
-                              selectedDays.add(entry.key);
-                            } else {
-                              selectedDays.remove(entry.key);
-                            }
-                          });
-                        },
-                      );
-                    }).toList(),
-                  ),
-                  const SizedBox(height: 16),
-                  ElevatedButton(
-                    onPressed: () async {
-                      final newRoutine = {
-                        'name': nameController.text,
-                        'description': descriptionController.text,
-                        'icon': iconController.text,
-                        'startDate': startDateController.text,
-                        'endDate': endDateController.text.isEmpty ? null : endDateController.text,
-                        'alert': null,
-                        'frequencyId': selectedFrequencyId,
-                        'recurrence': recurrence,
-                        'days': selectedDays.join(','),
-                      };
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      isEditing ? 'Modifier Routine' : 'Nouvelle Routine',
+                      style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 8),
+                    TextField(
+                      controller: nameController,
+                      decoration: const InputDecoration(labelText: 'Nom', border: OutlineInputBorder()),
+                    ),
+                    const SizedBox(height: 8),
+                    TextField(
+                      controller: descriptionController,
+                      decoration: const InputDecoration(labelText: 'Description', border: OutlineInputBorder()),
+                    ),
+                    const SizedBox(height: 8),
+                    TextField(
+                      controller: iconController,
+                      decoration: const InputDecoration(labelText: 'Icône', border: OutlineInputBorder()),
+                    ),
+                    const SizedBox(height: 8),
+                    TextField(
+                      controller: startDateController,
+                      decoration: const InputDecoration(labelText: 'Date de début', border: OutlineInputBorder()),
+                    ),
+                    const SizedBox(height: 8),
+                    TextField(
+                      controller: endDateController,
+                      decoration: const InputDecoration(labelText: 'Date de fin', border: OutlineInputBorder()),
+                    ),
+                    const SizedBox(height: 8),
+                    DropdownButtonFormField<int>(
+                      value: selectedFrequencyId,
+                      decoration: const InputDecoration(labelText: 'Fréquence', border: OutlineInputBorder()),
+                      items: frequencies.entries
+                          .map((entry) => DropdownMenuItem(
+                        value: entry.key,
+                        child: Text(entry.value),
+                      ))
+                          .toList(),
+                      onChanged: (value) {
+                        setModalState(() {
+                          selectedFrequencyId = value!;
+                        });
+                      },
+                    ),
+                    const SizedBox(height: 8),
+                    TextField(
+                      controller: TextEditingController(text: recurrence.toString()),
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(labelText: 'Récurrence', border: OutlineInputBorder()),
+                      onChanged: (value) {
+                        setModalState(() {
+                          recurrence = int.parse(value);
+                        });
+                      },
+                    ),
+                    const SizedBox(height: 8),
+                    Wrap(
+                      spacing: 8.0,
+                      children: daysOfWeek.entries.map((entry) {
+                        return ChoiceChip(
+                          label: Text(entry.value),
+                          selected: selectedDays.contains(entry.key),
+                          onSelected: (selected) {
+                            setModalState(() {
+                              if (selected) {
+                                selectedDays.add(entry.key);
+                              } else {
+                                selectedDays.remove(entry.key);
+                              }
+                            });
+                          },
+                        );
+                      }).toList(),
+                    ),
+                    const SizedBox(height: 8),
+                    ElevatedButton(
+                      onPressed: () async {
+                        if (nameController.text.isEmpty ||
+                            descriptionController.text.isEmpty ||
+                            selectedDays.isEmpty) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Veuillez remplir tous les champs obligatoires !'),
+                              backgroundColor: Colors.red,
+                            ),
+                          );
+                          return;
+                        }
 
-                      if (isEditing) {
-                        await routineProvider.updateRoutine(routine!['id'], newRoutine);
-                      } else {
-                        await routineProvider.addRoutine(newRoutine);
-                      }
+                        final newRoutine = {
+                          'name': nameController.text,
+                          'description': descriptionController.text,
+                          'icon': iconController.text,
+                          'startDate': startDateController.text,
+                          'endDate': endDateController.text.isEmpty ? null : endDateController.text,
+                          'alert': null,
+                          'frequencyId': selectedFrequencyId,
+                          'recurrence': recurrence,
+                          'days': selectedDays.join(','),
+                        };
 
-                      Navigator.pop(context);
-                    },
-                    style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
-                    child: Text(isEditing ? 'Modifier' : 'Ajouter'),
-                  ),
-                ],
+                        if (isEditing) {
+                          await routineProvider.updateRoutine(routine!['id'], newRoutine);
+                        } else {
+                          await routineProvider.addRoutine(newRoutine);
+                        }
+
+                        Navigator.pop(context);
+                      },
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                      child: Text(
+                        isEditing ? 'Modifier' : 'Ajouter',
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             );
           },
